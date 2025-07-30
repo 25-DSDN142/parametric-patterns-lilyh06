49573d1d144b1fb364a29862137ecf57656ebc24
lilyh06 <hagenlily@myvuw.ac.nz>
2025-07-30 17:17:32 +1200

    bird
    slightly better bird

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,25 +1,29 @@
 //your parameter variables go here!
 
-//beak
 let beakX1 = 125; //125// add additions in function//half beak thickness
 let beakY1 = 90; //90// add additions in function (size)
+
 let beakX2 = 125; //125//
 let beakY2 = 70; //70//
+
 let beakLengthX = 50; //50//
 let beakLengthY = 100;//100 & -40 from first line//TILT OF BEAK
-//HEAD
-//eye
+
+//head
 let X = 125 //125//
 let Y = 75 //175//
-let eyeSize = 15 //15//25//30// 
-let beakColourChange = 0;//chnage number for diff colour
 
+let eyeSize = 15 //15//25//30//
+
+let beakColourChange = 0;//chnage number for diff colour
+let headColourChange = 0;
+let lineColourChange = 0;
 
 //change beak angles, maybe neck lengths, colours and eye sizing, maybe head size and position?
 
 function setup_wallpaper(pWallpaper) {
-  //pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.output_mode(GRID_WALLPAPER);
+ pWallpaper.output_mode(DEVELOP_GLYPH);
+ // pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
@@ -35,25 +39,55 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+ angleMode(DEGREES)
+
  let myPurple = color(105, 23, 66);
  let myBlue = color(0, 0, 255);
- let myRed = color(255, 0, 0);
+ let myOrange = color(232, 126, 21);
 
  if(eyeSize>15){fill(255,0,0);}
 
- //BIRD 1
-  //NECK
+ //neck
  noStroke()
-fill(105, 23, 66)//purple
- rect(120,70,80,15)
+ fill(105, 23, 66);//purple 
+ rect(120,70,80,15);// top bird's neck, right side
  
-  noStroke()
- fill(105, 23, 66)//purple
- rect(0,70,45,15)
+ noStroke()
+ fill(105, 23, 66);//purple
+ rect(0,70,45,15);// top bird's neck, left side
+
+ noStroke()
+ fill(105, 23, 66);//purple
+ rect(0,120,80,15);// lower bird's neck
+
+ fill(105, 23, 66);//purple
+ rect(182,120,20,80);// lower bird's neck extention
+
+ noStroke()
+ fill(105, 23, 66);//purple
+ triangle (44.8,70,44.8,85,80,80);//top bird's neck END
+ rect(82,0,20,40);
+ triangle(82,39.8,102,39.8,92,55);//lower bird's neck END
 
  //BEAK
  noStroke()
  fill(105, 23, 66)//purple
+  if(beakColourChange == 0){
+    fill(myPurple);
+  }
+  else if(beakColourChange==1){
+    fill(myRed)
+  }
+  else{
+   fill(myOrange)
+  }
+//top bird's beak
+ triangle (beakX1,beakY1,beakX2,beakY2,beakLengthX,beakLengthY-40)//beak top
+ triangle (beakX1,beakY1,beakX2,beakY2,beakLengthX,beakLengthY)//beak bottom
+ 
+ noStroke()
+ fill(105, 23, 66)//purple
+
  if(beakColourChange == 0){
     fill(myPurple);
   }
@@ -61,16 +95,21 @@ fill(105, 23, 66)//purple
     fill(myRed)
   }
   else{
-fill(myBlue)
+   fill(myOrange)
   }
- triangle (beakX1,beakY1,beakX2,beakY2,beakLengthX,beakLengthY-40)//beak top
- triangle (beakX1,beakY1,beakX2,beakY2,beakLengthX,beakLengthY)//beak bottom
- 
+ //lower birds beak
+ //might need to make this independent from other  bird!!!!!!
+ triangle (beakX1-50,beakY1+50,beakX2-50,beakY2+50,beakLengthX+100,beakLengthY+10)//beak top
+ triangle (beakX1-50,beakY1+50,beakX2-50,beakY2+50,beakLengthX+100,beakLengthY+50)//beak bottom
  
  //HEAD
  fill (105, 23, 66)//purple
  noStroke()
- ellipse(X,Y,45)//head
+ ellipse(X,Y,45)//top head
+
+ fill (105, 23, 66)//purple
+ noStroke()
+ ellipse(X-50,Y+50,45)// lower head ///                changed
 
  //EYE
  if(eyeSize>15){fill(255,0,0);} //changes orange//can add more and comment out fill to change others
@@ -84,59 +123,37 @@ fill(myBlue)
  stroke(232, 126, 21)//orange ring
  noFill()
  ellipse(X,Y,eyeSize+15)
- 
- 
 
- //BIRD 2
-  //NECK
- noStroke()
- fill(105, 23, 66)//purple
- rect(0,120,80,15)
+ if(eyeSize>15){fill(255,0,0);} //changes orange//can add more and comment out fill to change others
+ else{fill(232, 126, 21)}
 
- //BEAK
- noStroke()
- fill(105, 23, 66)//purple
-
-if(beakColourChange == 0){
-    fill(myPurple);
-  }
-  else if(beakColourChange==1){
-    fill(myRed)
-  }
-  else{
-fill(myBlue)
-  }
- 
- 
- 
- triangle (beakX1-50,beakY1+50,beakX2-50,beakY2+50,beakLengthX+100,beakLengthY+10)//beak top
- triangle (beakX1-50,beakY1+50,beakX2-50,beakY2+50,beakLengthX+100,beakLengthY+50)//beak bottom
- 
- //HEAD
- fill (105, 23, 66)//purple
- noStroke()
- ellipse(X-50,Y+50,45)//head ///                changed
-
- //EYE
- fill (232, 126, 21)//orange
+ //fill (232, 126, 21)//orange
  ellipse(X-50,Y+50,eyeSize+10)//eyeball //      changed
  fill (105, 23, 66)//purple
  ellipse (X-50,Y+50,eyeSize)//inner eye//       changed
  strokeWeight(1)
  stroke(232, 126, 21)//orange ring
  noFill()
- ellipse(X-50,Y+50,eyeSize+15)//                changed
+ ellipse(X-50,Y+50,eyeSize+16)//                changed
+ 
 
 
- //Neck Extension
- fill(105, 23, 66)//purple neck extension
- rect(182,120,20,80)
+//lines
+  
+  if(lineColourChange == 0){
+    stroke(myPurple);
+  }
+  else if(beakColourChange==1){
+    stroke(myOrange)
+  }
+  else{
+   stroke(myBlue)
+  }
+ line(180,70,200,50)
 
- noStroke()
- fill(105, 23, 66)//purple
- triangle (45,70,45,85,80,80)
- rect(82,0,20,40)
- triangle(82,40,102,40,92,55)
+stroke(232, 126, 21)
+line (160,90,180,70)
+line (150,90,180,60)
 
 
 
