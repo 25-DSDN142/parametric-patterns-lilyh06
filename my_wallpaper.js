14e40735a29a975e7c2aa7b5841a34db08c7f9a8
lilyh06 <hagenlily@myvuw.ac.nz>
2025-08-04 16:40:57 +1200

    Update my_wallpaper.js
    Aug 4

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,36 +1,38 @@
 //your parameter variables go here!
 
-let tBeakX1 = 125; //125// add additions in function//half beak thickness
-let tBeakY1 = 90; //90// add additions in function (size)
-let tBeakX2 = 125; //125//
-let tBeakY2 = 70; //70//
-let tBeakLengthX = 50; //50// smaller=
-let tBeakLengthY = 100;//100 & -40 from first line//TILT OF BEAK
-let beakTiltUpper = 100;//60
+let tBeakX1 = 192; //125
+let tBeakY1 = 85; //90
+let tBeakX2 = 192; //125
+let tBeakY2 = 70; //70
 
-let lBeakX1 = 75; //75// add additions in function//half beak thickness
-let lBeakY1 = 140; //140//(130=thin) add additions in function (size) 
-let lBeakX2 = 75; //75//
+let tBeakLengthX = 50; //50// 
+let tBeakLengthY = 100;//100 //TILT OF BEAK
+let beakTiltUpper = 60;//60
+
+let lBeakX1 = 2; //75// add additions in function//half beak thickness
+let lBeakY1 = 135; //140//(130=thin) add additions in function (size) 
+let lBeakX2 = 2; //75//
 let lBeakY2 = 120; //120//
+
 let lBeakLengthX = 150; //150// smaller=smallerBeak
-let lBeakLengthY = 100;//110 & +40 from first line//TILT OF BEAK 
-let beakTiltLower = 100//150
+let lBeakLengthY = 110;//110 
+let beakTiltLower = 150;//150
 
-//head
-let X = 125 //125//
-let Y = 75 //75//
-let eyeSize = 0 //15//25//30//
-let headSize = 0 //45 //changes to blue if over 45
+let X = 22.5; //125
+let Y = 75; //75
 
-let beakColourChange = 2;//0=purple 1=blue 2=orange 3=brown //also controls inner eye
-let neckColourChange = 1;//0=purple  1=orange 2=brown else=blue
-let lineColourChange = 0;//0=purple 1=blue 2=orange else=brown//also controls clouds
-let cloudColourChange = 1;//0=purple 1=blue 2=orange else=brown
-let neckLines = 1;// 0=orange 1=purple 2=blue
-let ringColourChange = 0;//0=orange 1=blue else=brown
+let eyeSize = 25; //15//25//30//
+let headSize = 52;//50 //changes to blue if over 50
+let neckSize = 15;//15
 
+let beakColourChange = 1;//0=purple 1=blue 2=orange 3=brown // also controls inner eye
+let neckColourChange = 0;//0=purple  1=orange 2=brown else=blue
+let lineColourChange = 2;//0=purple 1=blue 2=orange else=brown
+let cloud1ColourChange = 0;//0=purple 1=blue 2=orange else=brown // lower
+let cloud2ColourChange = 1;//0=purple 1=blue 2=orange else=brown // upper
+let neckLines = 2;// 0=orange 1=purple 2=blue
+let ringColourChange = 1;//0=orange 1=blue 2=purple else=brown
 
-//change beak angles, maybe neck lengths, colours and eye sizing, maybe head size and position?
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -51,226 +53,199 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
  angleMode(DEGREES)
-
  let myPurple = color(105, 23, 66);
  let myBrown = color(173, 64, 0);
  let myOrange = color(232, 126, 21);
- let myBlue = color(61, 94, 166)
+ let myBlue = color(61, 94, 166);
 
- if(eyeSize>15){fill(255,0,0);}
-
- //neck
+//NECK
  noStroke()
  fill(105, 23, 66)//purple
   if(neckColourChange == 0){
     fill(myPurple);
   }
     else if(neckColourChange==1){
-    fill(myOrange)
+    fill(myOrange);
   }
    else if(neckColourChange==2){
-    fill(myBrown)
+    fill(myBrown);
   }
   else{
-   fill(myBlue)
+   fill(myBlue);
   }
- noStroke()
- //fill(105, 23, 66);//purple 
- rect(120,70,80,15);// top bird's neck, right side
- 
- noStroke()
- //fill(105, 23, 66);//purple
- rect(0,70,45,15);// top bird's neck, left side
-
- noStroke()
- //fill(105, 23, 66);//purple
- rect(0,120,80,15);// lower bird's neck
-
- //fill(105, 23, 66);//purple
- //rect(182,120,20,80);// lower bird's neck extention long
-
- noStroke()
- //fill(105, 23, 66);//purple
+ noStroke();
+ rect(120,70,80,neckSize);// top bird's neck, right side
+ rect(0,70,45,neckSize);// top bird's neck, left side
+ rect(0,120,80,neckSize);// lower bird's neck
  triangle (44.8,70,44.8,85,80,80);//top bird's neck END
- //rect(82,0,20,40);
- //triangle(82,39.8,102,39.8,92,55);//lower bird's neck long END
+ rect(150,120,51,neckSize);//lower bird neck extension
+ triangle(150.9,120,120,130,150,135);
 
- rect(150,120,51,15)//lower bird neck extension
- triangle(150.9,120,120,130,150,135)
-
- //BEAK
- noStroke()
+//BEAK
+ noStroke();
  fill(105, 23, 66)//purple
   if(beakColourChange == 0){
     fill(myPurple);
   }
   else if(beakColourChange==1){
-    fill(myBlue)
+    fill(myBlue);
   }
    else if(beakColourChange==2){
-    fill(myOrange)
+    fill(myOrange);
   }
-
   else{
-   fill(myBrown)
+   fill(myBrown);
   }
-//top bird's beak
- triangle (tBeakX1,tBeakY1,tBeakX2,tBeakY2,tBeakLengthX,beakTiltUpper)//beak top
- triangle (tBeakX1,tBeakY1,tBeakX2,tBeakY2,tBeakLengthX,tBeakLengthY)//beak bottom
- 
- noStroke()
- fill(105, 23, 66)//purple
+ triangle (tBeakX1,tBeakY1,tBeakX2,tBeakY2,tBeakLengthX,beakTiltUpper);//beak top, top bird
+ triangle (tBeakX1,tBeakY1,tBeakX2,tBeakY2,tBeakLengthX,tBeakLengthY);//beak bottom, top bird
+ noStroke();
+ fill(105, 23, 66);//purple
 
  if(beakColourChange == 0){
     fill(myPurple);
   }
   else if(beakColourChange==1){
-    fill(myBlue)
+    fill(myBlue);
   }
     else if(beakColourChange==2){
-    fill(myOrange)
-  }
-  else{
-   fill(myBrown)
-  }
- //lower birds beak
- //might need to make this independent from other  bird!!!!!!
- triangle (lBeakX1,lBeakY1,lBeakX2,lBeakY2,lBeakLengthX,lBeakLengthY)//beak top
- triangle (lBeakX1,lBeakY1,lBeakX2,lBeakY2,lBeakLengthX,beakTiltLower)//beak bottom
- 
- //HEAD
- if(headSize>45){fill(74, 117, 186);} //blue
- else{fill(105, 23, 66)}
-
- //fill (105, 23, 66)//purple
- noStroke()
- ellipse(X,Y,headSize)//top head
-
- //fill (105, 23, 66)//purple
- noStroke()
- ellipse(X-50,Y+50,headSize)// lower head ///                changed
-
- //EYE
- if(eyeSize>15){fill(130, 195, 232);} //changes orange//can add more and comment out fill to change others
- else{fill(232, 126, 21)}
-
- //fill (232, 126, 21)//orange
-
- ellipse(X,Y,eyeSize+10)//eyeball 
-
-if(beakColourChange == 0){
-    fill(myPurple);
-  }
-  else{
-   fill(myBrown)
-  }
-
- //fill (105, 23, 66)//purple
- ellipse (X,Y,eyeSize)//inner eye
- strokeWeight(2)
-
- 
-noFill()
-  if(eyeSize>15){fill(130, 195, 232);} //blue
- else{fill(232, 126, 21)}
-
- //fill (232, 126, 21)//orange
- ellipse(X-50,Y+50,eyeSize+10)//eyeball //      changed
-
-if(beakColourChange == 0){
-    fill(myPurple);
+    fill(myOrange);
   }
   else{
    fill(myBrown);
   }
-
- //fill (105, 23, 66)//purple
- ellipse (X-50,Y+50,eyeSize)//inner eye//       changed
+ triangle (lBeakX1,lBeakY1,lBeakX2,lBeakY2,lBeakLengthX,lBeakLengthY);//beak top, lower bird
+ triangle (lBeakX1,lBeakY1,lBeakX2,lBeakY2,lBeakLengthX,beakTiltLower);//beak bottom, lower bird
  
- if(eyeSize>15){stroke(130, 195, 232);} //blue
- else{stroke(232, 126, 21)}//orange
- //stroke(232, 126, 21)//orange ring
- noFill()
+//HEAD + EYE
+ if(headSize>50){fill(74, 117, 186);}//blue
+ else{fill(105, 23, 66)}
+ noStroke();
+ ellipse(X,Y,headSize);                //top head
+ ellipse(X-50,Y+50,headSize);           //lower head 
 
+ if(eyeSize>15){fill(130, 195, 232);}
+ else{fill(232, 126, 21)}
+ ellipse(X,Y,eyeSize+10);                 //eyeball 
+
+ if(beakColourChange == 0){
+    fill(myPurple);
+  }
+ else if(beakColourChange==1){
+    fill(myBlue);
+  }
+ else{
+   fill(myBrown);
+  }
+ ellipse (X,Y,eyeSize);                 //inner eye
+ strokeWeight(2);
+ noFill();
+
+  if(eyeSize>15){fill(130, 195, 232);}//blue
+ else{fill(232, 126, 21)}
+ ellipse(X-50,Y+50,eyeSize+10);         //eyeball
+
+ if(beakColourChange == 0){
+    fill(myPurple);
+  }
+ else if(beakColourChange==1){
+   fill(myBlue);
+  }
+ else{
+   fill(myBrown);
+  }
+ ellipse(X-50,Y+50,eyeSize);           //inner eye
+ 
+ if(eyeSize>15){stroke(130, 195, 232);}//blue
+ else{stroke(232, 126, 21)}//orange
+ noFill();
  
  if(ringColourChange == 0){
     stroke(myOrange);
   }
-  else if(ringColourChange==1){
-    stroke(myBlue)
+ else if(ringColourChange==1){
+    stroke(myBlue);
   }
-  else{
-   stroke(myBrown)//make good looking fun colour!!!!!!!!!!!!!!!
+   else if(ringColourChange==2){
+    stroke(myPurple);
   }
+ else{
+   stroke(myBrown);
+  }
+ ellipse(X,Y,eyeSize+16);              //top bird eye ring
+ ellipse(X-50,Y+50,eyeSize+16);        //bottom bird eye ring
 
- ellipse(X,Y,eyeSize+16)//                       top bird eye ring
- ellipse(X-50,Y+50,eyeSize+16)//                bottom bird eye ring
-
- 
-  
- 
- 
- //lines
-  if(neckLines==0){ stroke(232, 126, 21);   //orange
+//LINES
+ if(neckLines==0){ stroke(232, 126, 21);//orange
   } 
  else if(neckLines==1){
-    stroke(myPurple)
+    stroke(myPurple);
   }
- else{stroke(myBlue)
+ else{stroke(myBlue);
  }
  
- line (176,84,190,70)////rightline
- line (166,84,180,70)//middleline
- line (156,84,170,70)//leftline
+ line (176,84,190,70);//rightline
+ line (166,84,180,70);//middleline
+ line (156,84,170,70);//leftline
  
  if(lineColourChange == 0){
     stroke(myPurple);
   }
-  else if(lineColourChange==1){
-    stroke(myBlue)
+ else if(lineColourChange==1){
+    stroke(myBlue);
   }
-   else if(lineColourChange==2){
-    stroke(myOrange)
+ else if(lineColourChange==2){
+    stroke(myOrange);
   }
-  else{
-   stroke(myBrown)//make good looking fun colour!!!!!!!!!!!!!!!
+ else{
+   stroke(myBrown);
   }
- line(191,69,200,60)//rightline by neck
- line(181,69,200,50)//middleline by neck
- line(171,69,200,40)//leftline by neck
+ line(191,69,200,60);//rightline by neck
+ line(181,69,200,50);//middleline by neck
+ line(171,69,200,40);//leftline by neck
 
- line(0,40,40,0)//leftline top corner
- line(0,50,50,0)//middleline top corner
- line(0,60,60,0)//rightline bottom corner
+ line(0,40,40,0);//leftline top corner
+ line(0,50,50,0);//middleline top corner
+ line(0,60,60,0);//rightline bottom corner
 
- line(160,200,180,180)//rightline bottom corner
- line(150,200,178,172)//middleline bottom corner
- line(140,200,168,172)//leftline bottom corner
+ line(160,200,180,180);//rightline bottom corner
+ line(150,200,178,172);//middleline bottom corner
+ line(140,200,168,172);//leftline bottom corner
 
-
- //cloud
+//CLOUD
  noStroke()
- if(cloudColourChange == 0){
+ if(cloud1ColourChange == 0){
     fill(myPurple);
   }
-  else if(cloudColourChange==1){
-    fill(myBlue)
+ else if(cloud1ColourChange==1){
+    fill(myBlue);
   }
-   else if(cloudColourChange==2){
-    fill(myOrange)
+ else if(cloud1ColourChange==2){
+    fill(myOrange);
   }
-  else{
-   fill(myBrown)
+ else{
+   fill(myBrown);
   }
- arc(165, 185, 60, 20, 180, 0)//cloud base
- ellipse(175,175,15)//right top of bottomcloud
- ellipse(160,172,17)//middle top of bottomcloud
- ellipse(150,175,10)//left top of bottomcloud
+ arc(165, 185, 60, 20, 180, 0);//cloud base
+ ellipse(175,175,15);//right top of bottomcloud
+ ellipse(160,172,17);//middle top of bottomcloud
+ ellipse(150,175,10);//left top of bottomcloud
 
- arc(165, 25, 60, 20, 180, 0)//cloud base
- ellipse(170,15,15)//right top of bottomcloud
- ellipse(155,12,17)//middle top of bottomcloud
- ellipse(146,16,10)
+ if(cloud2ColourChange == 0){
+    fill(myPurple);
+  }
+ else if(cloud2ColourChange==1){
+    fill(myBlue);
+  }
+ else if(cloud2ColourChange==2){
+    fill(myOrange);
+  }
+ else{
+   fill(myBrown);
+  }
+ arc(165, 25, 60, 20, 180, 0);//cloud base
+ ellipse(170,15,15);//right top of bottomcloud
+ ellipse(155,12,17);//middle top of bottomcloud
+ ellipse(146,16,10);
 
 
 }
